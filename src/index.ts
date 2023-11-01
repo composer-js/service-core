33f7e884e5fd03d46caa1a12388da1b7d16b79b4
Jean-Philippe Steinmetz <jean-philippe@acceleratxr.com>
2023-10-31 17:41:56 -0700

    Adding missing exports to express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 export * from "./BackgroundService";
 export * from "./BackgroundServiceManager";
 export * from "./BulkError";
-export * from "./database/ConnectionManager";
-export * from "./passportjs/JWTStrategy";
 export * from "./Server";
 export * from "./NetUtils";
 export * from "./ObjectFactory";
+
+export * from "./database";
 export * from "./decorators";
+export * from "./express";
 export * from "./models";
+export * from "./passportjs";
 export * from "./routes";
